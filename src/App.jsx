30e5a8aaeb5a37998f20d8a5da1e9194a3a3e290
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-05-22 22:27:08 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Settings } from "@mui/icons-material"
+import { Button } from "@mui/material"
+
 function App() {
   return (
     <>
       <div>
         Hello World
       </div>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Button
+      </Button>
     </>
   )
 }
